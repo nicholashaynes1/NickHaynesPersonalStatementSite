4b294e59a44c295938d694484e5107d19ed810ad
nicholashaynes1 <nicholashaynes1@gmail.com>
2018-01-13 15:01:25 -0700

    Initail commit

---
+++ js/main.js
@@ -0,0 +1,44 @@
+function whoAmILoad()
+{
+	window.location = "C:/Users/Nick/Documents/WebsiteWorkspace/NickHaynesPersonalStatementSite/webPages/WhoIAm.html";
+};
+
+function whyChooseAWebsiteLoad()
+{
+	window.location = "C:/Users/Nick/Documents/WebsiteWorkspace/NickHaynesPersonalStatementSite/webPages/WhyChooseAWebsite.html";
+}
+
+function whyChooseMeLoad()
+{
+	window.location = "C:/Users/Nick/Documents/WebsiteWorkspace/NickHaynesPersonalStatementSite/webPages/WhyChooseMe.html";
+}
+
+function whyIWantToBeAnEngineerLoad()
+{
+	window.location = "C:/Users/Nick/Documents/WebsiteWorkspace/NickHaynesPersonalStatementSite/webPages/WhyIWantToBeAnEngineer.html";
+}
+
+function plansLoad()
+{
+	window.location = "C:/Users/Nick/Documents/WebsiteWorkspace/NickHaynesPersonalStatementSite/webPages/PostGradPlans.html";
+}
+
+function acomplishmentsLoad()
+{
+	window.location = "C:/Users/Nick/Documents/WebsiteWorkspace/NickHaynesPersonalStatementSite/webPages/Acomplishments.html";
+}
+
+function involvementLoad()
+{
+	window.location = "C:/Users/Nick/Documents/WebsiteWorkspace/NickHaynesPersonalStatementSite/webPages/Involvement.html";
+}
+
+function involvementLoad()
+{
+	window.location = "C:/Users/Nick/Documents/WebsiteWorkspace/NickHaynesPersonalStatementSite/webPages/ThankYou.html";
+}
+
+function backToTheStartLoad()
+{
+	window.location = "C:/Users/Nick/Documents/WebsiteWorkspace/NickHaynesPersonalStatementSite/index.html";
+}
